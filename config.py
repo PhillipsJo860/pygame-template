fc381f3f33e951239d9b0fda1179b4e0775f6078
SoggyCereal <PhillipsJo860@northwested.org>
2025-02-14 10:46:02 -0500

    Update the two project files

---
+++ config.py
@@ -0,0 +1,15 @@
+# Pygame Configuration Module
+
+# Color constants (RGB)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 155, 0)
+BLUE = (0, 0, 255)
+
+# Game window dimensions
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 600
+TITLE = 'TITLE'
+# Frame rate (frames per second)
+FPS = 60
